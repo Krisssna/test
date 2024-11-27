55cc20dc602742facaac5c5b0153ab4cb9ede27d
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-11-27 07:27:31 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  showSection("home");
+});
+
+function showSection(sectionId) {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach(section => section.classList.remove("active"));
+  document.getElementById(sectionId).classList.add("active");
+}
+
+function loadMoreBlogs() {
+  const blogContainer = document.querySelector(".blog-thumbnails");
+  for (let i = 5; i <= 8; i++) {
+    const blog = document.createElement("div");
+    blog.className = "blog-thumbnail";
+    blog.innerText = `Blog ${i}`;
+    blogContainer.appendChild(blog);
+  }
+}
+
+let playlistCount = 2;
+
+function loadMorePlaylists() {
+  const playlistContainer = document.getElementById("playlists");
+  const playlist = document.createElement("div");
+  playlist.className = "playlist";
+  playlist.innerHTML = `
+    <h3>Playlist ${playlistCount}</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL${playlistCount}" frameborder="0" allowfullscreen></iframe>
+  `;
+  playlistContainer.appendChild(playlist);
+  playlistCount++;
+}
